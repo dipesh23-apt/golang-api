f851fdaa32fdf85c19679ca5b3f7510da60c80cc
dipesh23-apt <80080241+dipesh23-apt@users.noreply.github.com>
2021-06-02 00:49:09 +0530

    Update user.go

--- controller/user.go
+++ controller/user.go
@@ -10,55 +10,59 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "apllication/json")
-	var b models.User
-	param := mux.Vars(r)
-	b, err := repo.GetUserfromDB(param["id"])
-	if err != nil {
-		fmt.Fprintf(w, "Invalid ID,does not exist")
-		return
+func GetUser() http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "apllication/json")
+		var b models.User
+		param := mux.Vars(r)
+		b, err := repo.GetUserfromDB(param["id"])
+		if err != nil {
+			fmt.Fprintf(w, "Invalid ID,does not exist")
+			return
+		}
+		json.NewEncoder(w).Encode(b)
 	}
-	json.NewEncoder(w).Encode(b)
-
 }
-func GetallUsers(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "application/json")
-	var x models.Body
-	json.NewDecoder(r.Body).Decode(&x)
-	c, err := repo.GetallUsersfromDB(x.Id)
-	if err != nil {
-		fmt.Fprintf(w, "Some id data cannot be found !!")
-		return
+func GetallUsers() http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var x models.Body
+		json.NewDecoder(r.Body).Decode(&x)
+		c, err := repo.GetallUsersfromDB(x.Id)
+		if err != nil {
+			fmt.Fprintf(w, "Some id data cannot be found !!")
+			return
+		}
+		json.NewEncoder(w).Encode(c)
 	}
-	json.NewEncoder(w).Encode(c)
 }
 
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var us models.User
-	err := json.NewDecoder(r.Body).Decode(&us)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+func CreateUser() http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var us models.User
+		err := json.NewDecoder(r.Body).Decode(&us)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		d, err := repo.CreateUserinDB(us)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		json.NewEncoder(w).Encode(d)
 	}
-	d, err := repo.CreateUserinDB(us)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	json.NewEncoder(w).Encode(d)
-
 }
-func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	param := mux.Vars(r)
-	err := repo.DeleteUserfromDB(param["id"])
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+func DeleteUser() http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		param := mux.Vars(r)
+		err := repo.DeleteUserfromDB(param["id"])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "Successfully deleted the user with id %s", param["id"])
 	}
-	fmt.Fprintf(w, "Successfully deleted the user with id %s", param["id"])
-
 }
