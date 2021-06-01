55c1169cbfc35cc65b39755bc98f3b8a6d4674b7
dipesh23-apt <80080241+dipesh23-apt@users.noreply.github.com>
2021-06-02 00:54:45 +0530

    Update execute.go

--- cmd/execute.go
+++ cmd/execute.go
@@ -11,11 +11,11 @@ import (
 
 func Execute() {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/user/{id}", controller.GetUser).Methods("GET")
-	r.HandleFunc("/api/v1/user/fetch", controller.GetallUsers).Methods("POST")
-	r.HandleFunc("/api/v1/user/create", controller.CreateUser).Methods("POST")
+	r.HandleFunc("/api/v1/user/{id}", controller.GetUser()).Methods("GET")
+	r.HandleFunc("/api/v1/user/fetch", controller.GetallUsers()).Methods("POST")
+	r.HandleFunc("/api/v1/user/create", controller.CreateUser()).Methods("POST")
 
-	r.HandleFunc("/api/v1/user/{id}", controller.DeleteUser).Methods("DELETE")
+	r.HandleFunc("/api/v1/user/{id}", controller.DeleteUser()).Methods("DELETE")
 	http.Handle("/", r)
 	fmt.Println("Running on server 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
