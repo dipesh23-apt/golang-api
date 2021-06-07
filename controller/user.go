1acb6278f9432d4f13c2cb14226f0223ed144253
dipesh23-apt <80080241+dipesh23-apt@users.noreply.github.com>
2021-06-07 10:44:59 +0530

    Add files via upload

---
+++ controller/user.go
@@ -0,0 +1,63 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/dipesh23-apt/golang_api/models"
+	"github.com/dipesh23-apt/golang_api/repo"
+	"github.com/gorilla/mux"
+)
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "apllication/json")
+	var b models.User
+	param := mux.Vars(r)
+	b, err := repo.GetUserfromDB(param["id"])
+	if err != nil {
+		fmt.Fprintf(w, "Invalid ID,does not exist")
+		return
+	}
+	json.NewEncoder(w).Encode(b)
+
+}
+func GetallUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var x models.Body
+	json.NewDecoder(r.Body).Decode(&x)
+	c, err := repo.GetallUsersfromDB(x.Id)
+	if err != nil {
+		fmt.Fprintf(w, "Some id data cannot be found !!")
+		return
+	}
+	json.NewEncoder(w).Encode(c)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var us models.User
+	err := json.NewDecoder(r.Body).Decode(&us)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	d, err := repo.CreateUserinDB(us)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	json.NewEncoder(w).Encode(d)
+
+}
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	param := mux.Vars(r)
+	err := repo.DeleteUserfromDB(param["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "Successfully deleted the user with id %s", param["id"])
+
+}
